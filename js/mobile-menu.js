f4266931c07ed6858f7c7fa1a69fed37a1ffb178
AziendaAgricolaNeri <orange1931007@gmail.com>
2026-03-26 15:37:11 +0100

    Cleaned JS files from merge markers

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,37 +1,28 @@
-<<<<<<< HEAD
-const mobileMenu = document.querySelector('.mobile-menu');
-const menuBtnOpen = document.querySelector('.menu-btn-open');
-const menuBtnClose = document.querySelector('.menu-btn-close');
-const menuLinks = document.querySelectorAll('.mobile-menu_link');
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
 
-const toggleMenu = () => mobileMenu.classList.toggle('is-open');
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
 
-// Відкриття/закриття меню
-menuBtnOpen.addEventListener('click', toggleMenu);
-menuBtnClose.addEventListener('click', toggleMenu);
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
 
-// Закриття меню при натисканні на посилання
-menuLinks.forEach(link => {
-  link.addEventListener('click', event => {
-    mobileMenu.classList.remove('is-open'); // Закриває меню
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Закриваємо мобільне меню при зміні орієнтації екрана
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
   });
-});
-=======
-const mobileMenu = document.querySelector('.mobile-menu');
-const menuBtnOpen = document.querySelector('.menu-btn-open');
-const menuBtnClose = document.querySelector('.menu-btn-close');
-const menuLinks = document.querySelectorAll('.mobile-menu_link');
-
-const toggleMenu = () => mobileMenu.classList.toggle('is-open');
-
-// Відкриття/закриття меню
-menuBtnOpen.addEventListener('click', toggleMenu);
-menuBtnClose.addEventListener('click', toggleMenu);
-
-// Закриття меню при натисканні на посилання
-menuLinks.forEach(link => {
-  link.addEventListener('click', event => {
-    mobileMenu.classList.remove('is-open'); // Закриває меню
-  });
-});
->>>>>>> 37c3a73d1697ffdcb84b31076eaa0caf98170ba1
+})();
