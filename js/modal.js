278e4635440784585b655404f1cef347d909def8
AziendaAgricolaNeri <orange1931007@gmail.com>
2026-03-26 15:08:47 +0100

    Fix JS files: remove merge markets

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const modal = document.querySelector('.backdrop');
 const modalBtnOpen = document.querySelectorAll('.modal-btn-open');
 const modalBtnClose = document.querySelector('.modal-btn-close');
@@ -7,13 +6,3 @@ const toggleModal = () => modal.classList.toggle('is-hidden');
 
 modalBtnOpen.forEach(btn => btn.addEventListener('click', toggleModal));
 modalBtnClose.addEventListener('click', toggleModal);
-=======
-const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelectorAll('.modal-btn-open');
-const modalBtnClose = document.querySelector('.modal-btn-close');
-
-const toggleModal = () => modal.classList.toggle('is-hidden');
-
-modalBtnOpen.forEach(btn => btn.addEventListener('click', toggleModal));
-modalBtnClose.addEventListener('click', toggleModal);
->>>>>>> 37c3a73d1697ffdcb84b31076eaa0caf98170ba1
